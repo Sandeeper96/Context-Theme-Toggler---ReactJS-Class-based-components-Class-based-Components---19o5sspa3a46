e162a0f29917d0ab4c477a43822edf3a2f612fed
Sandeeper96 <107251312+Sandeeper96@users.noreply.github.com>
2022-12-08 22:50:06 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { LocalThemedBox } from './LocalThemedBox';
+import { ThemeContext, ThemeProvider } from './ThemeProvider';
 
-const ThemeContext = React.createContext()
-const ThemeProvider = (props) =>{
+const Page = () => {
 
-    return (
-        <React.Fragment>
+    const {theme,setTheme} = useContext(ThemeContext);
 
-        </React.Fragment>
+    return(
+        <div className={'bg-'+theme} id="themed-page">
+            <p id="themed-text-container" className={'txt-'+theme}>
+                lorem ipsum dolor iterit n stuff
+            </p>
+            {/* 'btn btn-'+theme 'txt-'+theme */}
+            <button className={`btn btn-${theme} txt-${theme}`} id="themed-button">Themed Button</button>
+           
+        <LocalThemedBox/>
+           
+        </div>
     )
 }
 
-export {ThemeProvider,ThemeContext}
+export { Page }
